3adf58a681779767009991b2c7f94484fc829f72
Khant Hmue <khanthmu152uit@gmail.com>
2024-09-15 20:59:32 +0630

    modify url

--- deployment/urls.py
+++ deployment/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.http import HttpResponse
 
 def hello_world(request):
-    return HttpResponse("Hello world from Django!")
+    return HttpResponse("Hello world from Django updated!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
