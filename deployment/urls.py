2d107b69c6f3563b5a731594042fede328d6f8cb
Khant Hmue <khanthmu152uit@gmail.com>
2024-09-15 21:12:48 +0630

    cd test

--- deployment/urls.py
+++ deployment/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.http import HttpResponse
 
 def hello_world(request):
-    return HttpResponse("Hello world from Django updated!")
+    return HttpResponse("Hello world from Django updated 2, 3, blah!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
